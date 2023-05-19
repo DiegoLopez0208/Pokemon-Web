d6a663283dbd562197f50a29f9c3ebbe9e0cb1ad
DiegoLopez <lopezdiegog26@gmail.com>
2023-05-19 17:19:43 -0300

    Animation

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,19 @@
 let button = document.querySelector('.hiddenButton');
 let splashScreen = document.querySelector ('.splashScreen')
 
+let rectangle = document.querySelectorAll('.rectangle')
+const rectangle1 = document.querySelectorAll('.rectanglev2')
+const animationContainer = document.querySelector ('.animation-container')
+
 window.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', () => {
         console.log("¡Se presionó el botón!");
         splashScreen.classList.add('splash-screen-animation')
+        rectangle.forEach((element) => {
+        element.classList.add ('animationClose')})
+        rectangle1.forEach((element1) => {
+        element1.classList.add ('animationClose1')})
+        animationContainer.classList.add ('animationContainer')
         
     });
 });
