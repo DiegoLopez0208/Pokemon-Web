e798b62d7f0d934cef01c9b8a02fbc72d394f98e
DiegoLopez <lopezdiegog26@gmail.com>
2023-05-28 22:08:59 -0300

    Splash Screen + NavBar + Unify

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,62 @@
-let button = document.querySelector('.hiddenButton');
-let splashScreen = document.querySelector ('.splashScreen')
-
-let rectangle = document.querySelectorAll('.rectangle')
+//Main Splash Screen Buttons
+const button = document.querySelector('.hiddenButton');
+const pokeballButton = document.querySelector('.pokeball2');
+const pikachuButton = document.querySelector('.pikachu-button');
+// SplashScreen
+const splashScreen = document.querySelector ('.splashScreen')
+//Animation Container
+const rectangle = document.querySelectorAll('.rectangle')
 const rectangle1 = document.querySelectorAll('.rectanglev2')
 const animationContainer = document.querySelector ('.animation-container')
 
+
+
+//Hidden Button
 window.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', () => {
-        console.log("¡Se presionó el botón!");
         splashScreen.classList.add('splash-screen-animation')
         rectangle.forEach((element) => {
         element.classList.add ('animationClose')})
         rectangle1.forEach((element1) => {
         element1.classList.add ('animationClose1')})
         animationContainer.classList.add ('animationContainer')
-        
+        setTimeout(() => {
+            splashScreen.remove()
+        }, 9000);
+        setTimeout(() => {
+            splashScreen.classList.add ('remove')
+            animationContainer.classList.add('animationClose2')
+        }, 3000);
     });
+});
+//Pikachu Button
+pikachuButton.addEventListener('click', () => {
+    splashScreen.classList.add('splash-screen-animation')
+    rectangle.forEach((element) => {
+    element.classList.add ('animationClose')})
+    rectangle1.forEach((element1) => {
+    element1.classList.add ('animationClose1')})
+    animationContainer.classList.add ('animationContainer')
+    setTimeout(() => {
+        splashScreen.classList.add ('remove')
+        animationContainer.classList.add('animationClose2')
+    }, 2000);
+    setTimeout(() => { window.location.href = "../pages/pokedex.html";
+}, 4700);
+});
+//Pokeball Button
+pokeballButton.addEventListener('click', () => {
+    splashScreen.classList.add('splash-screen-animation')
+    rectangle.forEach((element) => {
+    element.classList.add ('animationClose')})
+    rectangle1.forEach((element1) => {
+    element1.classList.add ('animationClose1')})
+    animationContainer.classList.add ('animationContainer')
+    setTimeout(() => {
+        splashScreen.classList.add ('remove')
+        animationContainer.classList.add('animationClose2')
+    }, 2000);
+    setTimeout(() => { window.location.href = "../pages/pokeball.html";
+}, 4700);
+
 });
---
+++ javascript/script2.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector (".hamburger")
+const navMenu = document.querySelector (".nav-menu")
+hamburger.addEventListener ("click", ()=> {
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=> {
+navMenu.classList.remove("active")
+hamburger.classList.remove("active")
+}))
