ffb2275c39d25e0780556ae650848820915b88a4
Matias Nicolas Quintana <matiasnicolasquintana09@gmail.com>
2023-05-31 20:28:29 -0300

    code  actualizated

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,33 @@
+const navMenu = document.querySelector("#nav");
 
-const navMenu = document.querySelector('#nav');
+const openMenu = document.querySelector(".open-menu");
 
-const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector("#close");
 
-const closeMenu = document.querySelector('#close');
+window.addEventListener("DOMContentLoaded", () => {
+  openMenu.addEventListener("click", () => {
+    navMenu.classList.add("visible");
+  });
 
-window.addEventListener('DOMContentLoaded', () => {
+  closeMenu.addEventListener("click", () => {
+    navMenu.classList.remove("visible");
+  });
+});
 
-openMenu.addEventListener("click", () => {
-    navMenu.classList.add("visible")
-})
+const nameInput = document.getElementById("name");
 
-closeMenu.addEventListener("click", () => {
-    navMenu.classList.remove("visible")
-})
+const lastNameInput = document.querySelector(".last-name");
 
-})
+const pokemonAdd = document.querySelector(".pokemon-add");
 
+const typePokemon = document.querySelector(".type-pokemon");
+
+const generationPokemon = document.querySelector(".generation-pokemon");
+
+const idPokemon = document.querySelector(".id-pokemon");
+
+nameInput.oninvalid = function (event) {
+  event.target.setCustomValidity(
+    "Username should only contain lowercase letters. e.g. nicolas"
+  );
+};
