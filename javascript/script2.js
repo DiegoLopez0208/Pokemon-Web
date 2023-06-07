d572d4ca0ddc0d425ce165fba9c314f066341c77
DiegoLopez <lopezdiegog26@gmail.com>
2023-06-07 19:50:17 -0300

    LocalStorage Fixed / Added New Pokemons (Format WEBP) / Removed All Useles Images

--- javascript/script2.js
+++ javascript/script2.js
@@ -8,3 +8,95 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",(
 navMenu.classList.remove("active")
 hamburger.classList.remove("active")
 }))
+
+//Formulary Settings
+
+const inputOne = document.getElementById("name");
+const lastNameInput = document.getElementById("last-name");
+const pokemonAdd = document.getElementById("pokemon-add");
+const typePokemon = document.getElementById("type-pokemon");
+const generationPokemon = document.getElementById("generation-pokemon");
+const idPokemon = document.getElementById("id-pokemon");
+const buttonSend = document.getElementById ('button-submit');
+const regExp = new RegExp(/[a-zA-Z]$/);
+const spanInput = document.getElementById ('input-incorrect')
+
+inputOne.addEventListener("input", () => {
+if(!regExp.test(inputOne.value)){
+spanInputOne.classList.remove("input-incorrect2")
+}else if(regExp.test(inputOne.value === regExp)){
+    spanInput.classList.add('input-incorrect2')
+}
+});
+lastNameInput.addEventListener("input", () => {
+if(!regExp.test(lastNameInput.value)){
+spanInputTwo.classList.remove("input-incorrect2")
+}else if(regExp.test(inputOne.value === regExp)){
+    spanInput.classList.add('input-incorrect2')
+}
+});
+pokemonAdd.addEventListener("input", () => {
+if(!regExp.test(pokemonAdd.value)){
+spanInputThree.classList.remove("input-incorrect2")
+}else if(regExp.test(pokemonAdd.value === regExp)){
+    spanInput.classList.add('input-incorrect2')
+}
+});
+typePokemon.addEventListener("input", () => {
+if(!regExp.test(typePokemon.value)){
+spanInput.classList.remove("input-incorrect2")
+}
+else if(regExp.test(typePokemon.value === regExp)){
+    spanInput.classList.add('input-incorrect2')
+}
+});
+generationPokemon.addEventListener("input", () => {
+if(!regExp.test(generationPokemon.value)){
+    spanInput.classList.remove("input-incorrect2")
+}else if(regExp.test(generationPokemon.value === regExp)){
+    spanInput.classList.add('input-incorrect2')
+}
+});
+idPokemon.addEventListener("input", () => {
+    if(!regExp.test(idPokemon.value)){
+        spanInput.classList.remove("input-incorrect2")
+    }else if(regExp.test(idPokemon.value === regExp)){
+        spanInput.classList.add('input-incorrect2')
+    }
+    });
+
+buttonSend.addEventListener("click", () => {
+    localStorage.setItem("name", JSON.stringify(inputOne.value))
+    JSON.parse(localStorage.getItem("name"))
+
+});
+
+buttonSend.addEventListener("click", () => {
+    localStorage.setItem("lastname", JSON.stringify(lastNameInput.value))
+    JSON.parse(localStorage.getItem("lastname"))
+
+});
+
+buttonSend.addEventListener("click", () => {
+    localStorage.setItem("pokemonadd", JSON.stringify(pokemonAdd.value))
+    JSON.parse(localStorage.getItem("pokemonadd"))
+
+});
+
+buttonSend.addEventListener("click", () => {
+    localStorage.setItem("typepokemon", JSON.stringify(typePokemon.value))
+    JSON.parse(localStorage.getItem("typepokemon"))
+})
+    
+
+buttonSend.addEventListener("click", () => {
+    localStorage.setItem("generationpokemon", JSON.stringify(generationPokemon.value))
+    JSON.parse(localStorage.getItem("generationpokemon"))
+    
+});
+
+buttonSend.addEventListener("click", () => {
+    localStorage.setItem("idPokemon", JSON.stringify(idPokemon.value))
+    JSON.parse(localStorage.getItem("idPokemon"))
+    
+})
--- javascript/splashscreen.js
+++ javascript/splashscreen.js
@@ -10,25 +10,26 @@ const rectangle1 = document.querySelectorAll('.rectanglev2')
 const animationContainer = document.querySelector ('.animation-container')
 
 
-
+const waitFor = (ms) => {
+    return new Promise(resolve => setTimeout (resolve,ms))
+}
 //Hidden Button
+// Clarification: It was not done due to lack of time, the idea of playing a turn-based game (Pokemon Player Vs Bot Combat Style)
+
+  //  button.addEventListener('click', () => {
+    //    splashScreen.classList.add('splash-screen-animation')
+      //  rectangle.forEach((element) => {
+      //  element.classList.add ('animationClose')})
+      //  rectangle1.forEach((element1) => {
+      //  element1.classList.add ('animationClose1')})
+      //  animationContainer.classList.add ('animationContainer')
+       // setTimeout(() => {
+        //    splashScreen.classList.add ('remove')
+         //   animationContainer.classList.add('animationClose2')
+       // }, 3000);
+   // });
+// });
 window.addEventListener('DOMContentLoaded', () => {
-    button.addEventListener('click', () => {
-        splashScreen.classList.add('splash-screen-animation')
-        rectangle.forEach((element) => {
-        element.classList.add ('animationClose')})
-        rectangle1.forEach((element1) => {
-        element1.classList.add ('animationClose1')})
-        animationContainer.classList.add ('animationContainer')
-        setTimeout(() => {
-            splashScreen.remove()
-        }, 9000);
-        setTimeout(() => {
-            splashScreen.classList.add ('remove')
-            animationContainer.classList.add('animationClose2')
-        }, 3000);
-    });
-});
 //Pikachu Button
 pikachuButton.addEventListener('click', () => {
     splashScreen.classList.add('splash-screen-animation')
@@ -37,12 +38,14 @@ pikachuButton.addEventListener('click', () => {
     rectangle1.forEach((element1) => {
     element1.classList.add ('animationClose1')})
     animationContainer.classList.add ('animationContainer')
-    setTimeout(() => {
+    waitFor(2000).then(()=>{
         splashScreen.classList.add ('remove')
         animationContainer.classList.add('animationClose2')
-    }, 2000);
-    setTimeout(() => { window.location.href = "../pages/pokedex.html";
-}, 4700);
+        return waitFor(3000)
+    })
+    .then(()=>{
+        window.location.href = "../pages/pokedex.html";
+    })
 });
 //Pokeball Button
 pokeballButton.addEventListener('click', () => {
@@ -52,11 +55,15 @@ pokeballButton.addEventListener('click', () => {
     rectangle1.forEach((element1) => {
     element1.classList.add ('animationClose1')})
     animationContainer.classList.add ('animationContainer')
-    setTimeout(() => {
+    waitFor(2000).then(()=>{
         splashScreen.classList.add ('remove')
         animationContainer.classList.add('animationClose2')
-    }, 2000);
-    setTimeout(() => { window.location.href = "../pages/pokeball.html";
-}, 4700);
+        return waitFor(3000)
+    })
+    .then(()=>{
+        window.location.href = "../pages/pokeball.html";
+    })
+
 
 });
+})
